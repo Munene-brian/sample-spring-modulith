6e9374b5f50ef37c4b92e5be88a6406ca059594d
Munene Brian <52892865+Munene-brian@users.noreply.github.com>
2024-03-21 09:50:07 +0300

    first test

--- src/main/java/pl/piomin/services/SpringModulith.java
+++ src/main/java/pl/piomin/services/SpringModulith.java
@@ -3,7 +3,10 @@ package pl.piomin.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
+
 public class SpringModulith {
 
     public static void main(String[] args) {
